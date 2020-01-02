99004cf9fa9a4cf75703f1f28d7f43bbbbf05037
jgersain <jhersein@gmail.com>
2020-01-02 00:03:34 -0600

    Add main page path

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,17 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import HomePage from '../views/HomePage.vue';
+
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: '/',
+    name: 'HomePage',
+    component: HomePage,
+  },
+];
 
 const router = new VueRouter({
   mode: 'history',
